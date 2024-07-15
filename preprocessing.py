f6ebf81702d422e3f99eac8c495509cad16e51e5
Naught0 <wasiuddin2007@gmail.com>
2024-07-16 01:01:45 +0600

    preprocessing.py can remove problems above 15 and save the file

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,12 @@
 import pandas as pd
 
-data = pd.from_csv("C:\Users\Wasiu\OneDrive\Desktop\Projects\MehtaPlus\ai-math-solver\parsed_ArtOfProblemSolving.csv")
+data = pd.read_csv("C:\\Users\\Wasiu\\OneDrive\\Desktop\\Projects\\MehtaPlus\\ai-math-solver\\parsed_ArtOfProblemSolving.csv")      #   REPLACE WITH YOUR OWN FILEPATH
 
-print(data)
+data = data.drop(columns=['problem_id', 'answer'])
+
+data['problem_no'] = data['link'].apply(lambda x: int(x.split("_")[-1]))
+data['test'] = data['link'].apply(lambda x: x.split("/")[-2])
+
+data = data.drop(data[data.problem_no > 15].index)
+
+data.to_csv("cleaned_ArtOfProblemSolving.csv")
